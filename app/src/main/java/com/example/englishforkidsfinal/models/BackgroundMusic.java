f715d55e80ce9b7e51320230e0c96128a20d4d00
radkoff26 <radko.slava@mail.ru>
2021-02-28 18:19:16 +0300

    background music has been managed to switch song up after the end of playing

--- app/src/main/java/com/example/englishforkidsfinal/models/BackgroundMusic.java
+++ app/src/main/java/com/example/englishforkidsfinal/models/BackgroundMusic.java
@@ -12,6 +12,7 @@ public class BackgroundMusic extends Thread {
     private List<Integer> tracks;
     private MediaPlayer mp;
     private Context ctx;
+    private boolean isPlaying;
 
     public BackgroundMusic(List<Integer> tracks, Context ctx) {
         this.tracks = tracks;
@@ -27,17 +28,40 @@ public class BackgroundMusic extends Thread {
     @Override
     public void run() {
         mp.start();
+        isPlaying = true;
+        while (isPlaying) {
+            if (mp.getCurrentPosition() + 10 > mp.getDuration()) {
+                nextTrack();
+            }
+        }
     }
 
     public void pause() {
+        isPlaying = false;
         if (mp != null && mp.isPlaying()) {
             mp.pause();
         }
     }
 
     public void resumeMusic() {
+        isPlaying = true;
         if (mp != null && !mp.isPlaying()) {
             mp.start();
         }
+    }
+
+    public void stopPlaying() {
+        isPlaying = false;
+    }
+
+    // test void
+    public void nextTrack() {
+        int id = idOfTrack;
+        while (idOfTrack == id) {
+            randomizeTrack();
+        }
+        mp.pause();
+        mp = MediaPlayer.create(ctx, tracks.get(idOfTrack));
+        mp.start();
     }
 }
